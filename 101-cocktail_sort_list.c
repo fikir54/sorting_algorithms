461d522b661ff43ba4346594d4aa3e30b4b68a1a
fikir54 <fikir.page@gmail.com>
2022-07-15 20:34:44 +0300

    Cocktail Sort Lists

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,64 +1,91 @@
 #include "sort.h"
-
 /**
- * swap - swaps two nodes
- * @head: head of the list
- * @node1: first node to sort
- * @node2: second node to sort
+ * swap1 - swaps nodes from left to right
+ * @list: pointer to list
+ * @head: pointer to head node
+ * @aux: auxiliar pointer
+ * Return: no return
  */
-void swap(listint_t **head, listint_t *node1, listint_t *node2)
+void swap1(listint_t **list, listint_t *head, listint_t *aux)
 {
-	listint_t *prev, *next;
-
-	prev = node1->prev;
-	next = node2->next;
-
-	if (prev != NULL)
-		prev->next = node2;
+	if (head->prev)
+		head->prev->next = aux;
 	else
-		*head = node2;
-	node1->prev = node2;
-	node1->next = next;
-	node2->prev = prev;
-	node2->next = node1;
-	if (next)
-		next->prev = node1;
+		*list = aux;
+	if (aux->next)
+		aux->next->prev = head;
+	head->next = aux->next;
+	aux->prev = head->prev;
+	aux->next = head;
+	head->prev = aux;
+	print_list(*list);
+
 }
 /**
- * cocktail_sort_list - sorts a list using the cocktail sort algorithm
- * @list: list to sort
+ * swap2 - swaps nodes from right to left
+ * @list: pointer to list
+ * @head: pointer to head node
+ * @aux: auxiliar pointer
+ * Return: no return
  */
+void swap2(listint_t **list, listint_t *head, listint_t *aux)
+{
+	aux = head->prev;
+	aux->next->prev = aux->prev;
+	if (aux->prev)
+		aux->prev->next = aux->next;
+	else
+		*list = aux->next;
+	aux->prev = aux->next;
+	aux->next = aux->next->next;
+	aux->prev->next = aux;
+	if (aux->next)
+		aux->next->prev = aux;
+	print_list(*list);
+}
+
+/**
+ * cocktail_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Cocktail sort ailgorithm
+ * @list: pointer to the list head
+ * Return: no return
+ **/
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *head;
-	int flag = 0;
+	listint_t *head, *aux;
+	int flag = 1;
 
-	if (!list || !*list || !(*list)->next)
-		return;
+	if (list)
+	{
+		head = *list;
+		while (flag != 0)
+		{
+			flag = 0;
+			while (head->next)
+			{
+				if (head->n > head->next->n)
+				{
+					aux = head->next;
+					swap1(list, head, aux);
+					flag = 1;
+				}
+				else
+					head = head->next;
+			}
+			if (flag == 0)
+				break;
+			flag = 0;
+			while (head->prev)
+			{
+				if (head->prev->n > head->n)
+				{
+					swap2(list, head, aux);
+					flag = 1;
+				}
+				else
+					head = head->prev;
+			}
 
-	do {
-		for (head = *list; head->next != NULL; head = head->next)
-		{
-			if (head->n > head->next->n)
-			{
-				swap(list, head, head->next);
-				print_list(*list);
-				flag = 1;
-				head = head->prev;
-			}
 		}
-		if (flag == 0)
-			break;
-		flag = 0;
-		for (; head->prev != NULL; head = head->prev)
-		{
-			if (head->n < head->prev->n)
-			{
-				swap(list, head->prev, head);
-				print_list(*list);
-				flag = 1;
-				head = head->next;
-			}
-		}
-	} while (flag == 1);
+	}
 }
